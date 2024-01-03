0e649bebfdd48954e58ab41ea7f7709aebdecce1
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2024-01-03 19:46:49 -0300

    feat: create basic drag functionality

--- src/lib/components/SortableItem.svelte
+++ src/lib/components/SortableItem.svelte
@@ -1,10 +1,56 @@
-<li class="sortable-item" tabindex="0">
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
+	import type { IDragging, IItemClone } from './SortableList.svelte';
+
+	export let index: number | undefined = undefined;
+
+	let itemRef: HTMLLIElement;
+
+	const dragging = getContext<Writable<IDragging>>('dragging');
+	const itemClone = getContext<Writable<IItemClone | null>>('itemClone');
+
+	function handleMouseDown(event: MouseEvent) {
+		const currTarget = event.currentTarget as HTMLLIElement;
+
+		$dragging = { isActive: true, itemIndex: index ?? null };
+		$itemClone = {
+			text: currTarget?.textContent || '',
+			width: currTarget?.getBoundingClientRect().width,
+			height: currTarget?.getBoundingClientRect().height,
+			left: currTarget?.getBoundingClientRect().left,
+			top: currTarget?.getBoundingClientRect().top,
+			clientX: event.clientX,
+			clientY: event.clientY,
+		};
+	}
+</script>
+
+<li
+	bind:this={itemRef}
+	class="sortable-item {$$restProps.class ? $$restProps.class : ''}"
+	class:is-dragging={$dragging.itemIndex === index}
+	style={$$restProps.style ? $$restProps.style : ''}
+	tabindex="0"
+	on:mousedown={handleMouseDown}
+>
 	<slot />
 </li>
 
-<style>
+<style lang="scss">
 	.sortable-item {
+		box-sizing: border-box;
 		list-style: none;
 		cursor: grab;
+		user-select: none;
+
+		&--ghost {
+			position: fixed;
+			cursor: grabbing;
+		}
+
+		&.is-dragging {
+			visibility: hidden;
+		}
 	}
 </style>
--- src/lib/components/SortableList.svelte
+++ src/lib/components/SortableList.svelte
@@ -1,9 +1,78 @@
+<script lang="ts" context="module">
+	export interface IDragging {
+		isActive: boolean;
+		itemIndex: number | null;
+	}
+
+	export interface IItemClone {
+		text: string;
+		width: number;
+		height: number;
+		left: number;
+		top: number;
+		clientX: number;
+		clientY: number;
+		transform?: string;
+	}
+</script>
+
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+	import SortableItem from './SortableItem.svelte';
+
+	const dragging = writable<IDragging>({
+		isActive: false,
+		itemIndex: null,
+	});
+	const itemClone = writable<IItemClone | null>(null);
+	setContext('dragging', dragging);
+	setContext('itemClone', itemClone);
+
+	function handleMouseMove(event: MouseEvent) {
+		if ($dragging.isActive) {
+			$itemClone = {
+				...$itemClone,
+				transform: `translate3d(${event.clientX - ($itemClone?.clientX || 0)}px, ${
+					event.clientY - ($itemClone?.clientY || 0)
+				}px, 0)`,
+			} as IItemClone;
+		}
+	}
+
+	function handleMouseUp(event: MouseEvent) {
+		$itemClone = {
+			...$itemClone,
+			transform: 'translate3d(0, 0, 0)',
+		} as IItemClone;
+		$dragging = { isActive: false, itemIndex: null };
+	}
+
+	$: itemCloneStyles =
+		!!$itemClone &&
+		[
+			`width: ${$itemClone.width}px`,
+			`height: ${$itemClone.height}px`,
+			`left: ${$itemClone.left}px`,
+			`top: ${$itemClone.top}px`,
+			`transform: ${$itemClone.transform}`,
+		].join(';');
+</script>
+
+<svelte:document on:mousemove={handleMouseMove} on:mouseup={handleMouseUp} />
+
 <ul class="sortable-list">
 	<slot />
+	{#if $dragging.itemIndex !== null && $itemClone}
+		<SortableItem class="sortable-item--ghost" style={itemCloneStyles}>
+			{$itemClone.text}
+		</SortableItem>
+	{/if}
 </ul>
 
 <style>
 	.sortable-list {
+		box-sizing: border-box;
 		padding: 0;
 	}
 </style>
